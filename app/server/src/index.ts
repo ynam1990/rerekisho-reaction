9249a78d95c14b8e34586b570102f057fae00de6
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-07 09:44:25 +0900

    [update] expressからreactファイルを返却できるようにする

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const APP_PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.send('Hello!')
+app.use(express.static(
+  path.join(__dirname, '../../client/dist')
+));
+
+app.all('*', (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(404).json({
+      error: `No api found for ${ req.path }`
+    });
+  }
+  
+  res.sendFile(
+    path.join(__dirname, '../../client/dist', 'index.html')
+  );
 });
 
 app.listen(APP_PORT, () => {
