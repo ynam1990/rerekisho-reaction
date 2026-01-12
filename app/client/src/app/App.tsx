37011a0bca699710315b03de9a7b29a9e07a5303
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-12 23:08:13 +0900

    [update] HeaderとFooterの配置

--- app/client/src/app/App.tsx
+++ app/client/src/app/App.tsx
@@ -2,12 +2,24 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from '@/shared/styles/theme'
 import { GlobalStyle } from '@/shared/styles/GlobalStyle';
 import { AppRouter } from './router/AppRouter'
+import { AppWrapper } from './App.styles';
+import { Header } from '@/shared/ui/organisms/Header';
+import { Footer } from '@/shared/ui/organisms/Footer';
 
 export const App = () => {
   return (
     <ThemeProvider theme={ theme }>
       <GlobalStyle />
-      <AppRouter />
+
+      <AppWrapper>
+        <Header isAuthenticated={ false } />
+        
+        <main>
+          <AppRouter />
+        </main>
+        
+        <Footer />
+      </AppWrapper>
     </ThemeProvider>
   );
 };
