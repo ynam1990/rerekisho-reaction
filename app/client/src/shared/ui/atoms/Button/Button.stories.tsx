3128efa6494688066d4176f44e5ade590b213919
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-12 16:10:42 +0900

    [update] Buttonのスタイル追加

--- app/client/src/shared/ui/atoms/Button/Button.stories.tsx
+++ app/client/src/shared/ui/atoms/Button/Button.stories.tsx
@@ -1,11 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from './Button';
+import { Button, BUTTON_TYPES } from './Button';
+import { theme } from '@/shared/styles/theme';
 
 const meta: Meta<typeof Button> = {
   title: 'Atoms/Button',
   component: Button,
   args: {
     children: 'Button',
+    type:'default',
+    size: 'md',
+  },
+  argTypes: {
+    type: {
+      control: { type: 'select' },
+      options: BUTTON_TYPES,
+    },
+    size: {
+      control: { type: 'select' },
+      options: Object.keys(theme.typography.fontSize),
+    },
   },
 };
 
--- app/client/src/shared/ui/atoms/Button/Button.tsx
+++ app/client/src/shared/ui/atoms/Button/Button.tsx
@@ -1,5 +1,109 @@
-import styled from 'styled-components'
+import type { theme, WithTheme } from '@/shared/styles/theme';
+import styled, { css, type Interpolation } from 'styled-components'
 
-export const Button = styled.button`
-  font-size: ${ ({ theme }) => theme.typography.fontSize.lg2 };
+export const BUTTON_TYPES = [
+  'default',
+  'success',
+  'proceed',
+  'cancel',
+  'danger',
+] as const;
+export type ButtonTypes = typeof BUTTON_TYPES[number];
+
+type Props = {
+  type?: ButtonTypes;
+  size?: keyof typeof theme.typography.fontSize;
+};
+type PropsWithTheme = WithTheme<Props>;
+
+const mediaQueryStyle = ({ theme, size = 'md' }: PropsWithTheme): Interpolation<Props> => {
+  const fontSize = theme.typography.fontSize[size];
+  const letterSpacingBody = theme.typography.letterSpacing.body;
+  const lineHeight = theme.typography.lineHeight;
+  const spacing = theme.spacing;
+  
+  return css`
+    font-size: ${ fontSize.pc };
+    letter-spacing: ${ letterSpacingBody.pc };
+    line-height: ${ lineHeight.tight.pc };
+    padding: ${ `${ spacing.md.pc } ${ spacing.lg.pc }` };
+    
+    @media (max-width: ${ theme.breakpoints.sp}) {
+      font-size: ${ fontSize.sp };
+      letter-spacing: ${ letterSpacingBody.sp };
+      line-height: ${ lineHeight.tight.sp };
+      padding: ${ `${ spacing.md.sp } ${ spacing.lg.sp }` };
+    }
+  `;
+};
+
+export const Button = styled.button<Props>`
+  font-weight: bold;
+  border-radius: ${ ({ theme }) => theme.radius.lg };
+  cursor: pointer;
+  transition:
+    box-shadow 0.15s ease,
+    transform 0.15s ease,
+    background-color 0.15s ease;
+
+  ${ mediaQueryStyle }
+
+  ${ ({ theme, type = 'default' }) => {
+    const color = theme.color;
+
+    switch (type) {
+      case 'success': {
+        return css`
+          color: ${ color.white };
+          border: 1px solid ${ color.paperIvory };
+          background-color: ${ color.successGreen };
+        `;
+      }
+      case 'proceed': {
+        return css`
+          color: ${ color.white };
+          border: 1px solid ${ color.paperIvory };
+          background-color: ${ color.proceedBlue };
+        `;
+      }
+      case 'cancel': {
+        return css`
+          color: ${ color.white };
+          border: 1px solid ${ color.paperIvory };
+          background-color: ${ color.cancelGray };
+        `;
+      }
+      case 'danger': {
+        return css`
+          color: ${ color.white };
+          border: 1px solid ${ color.paperIvory };
+          background-color: ${ color.dangerRed };
+        `;
+      }
+      default: {
+        return css`
+          color: ${ color.inkBlack };
+          border: 1px solid ${ color.inkBlack };
+          background-color: ${ color.paperIvory };
+        `;
+      }
+    }
+  } }
+
+  &:hover {
+    opacity: ${ ({ theme }) => theme.opacity.hover };
+  }
+
+  &:active {
+    transform: translateY(2px);
+  }
+
+  &:focus-visible {
+    border-width: 2px;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: ${ ({ theme }) => theme.opacity.disabled };
+  }
 `;
