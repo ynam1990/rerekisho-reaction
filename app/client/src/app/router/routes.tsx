c92cd7e4cde4ee1ee91862548bdc4442a1641440
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-13 14:56:18 +0900

    [update] 存在しないURLをホーム画面へリダイレクトするよう変更

--- app/client/src/app/router/routes.tsx
+++ app/client/src/app/router/routes.tsx
@@ -1,6 +1,7 @@
-import type { RouteObject } from 'react-router-dom';
+import { Navigate, type RouteObject } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage'
 
 export const routes: RouteObject[] = [
   { path: '/', element: <HomePage /> },
+  { path: '*', element: <Navigate to="/" replace /> },
 ];
