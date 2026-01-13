140e6f4e1814e64fb51785121e9cd8f9ece53302
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-13 13:43:23 +0900

    [update] Header左上のロゴ表示

--- app/client/src/shared/ui/organisms/Header/Header.tsx
+++ app/client/src/shared/ui/organisms/Header/Header.tsx
@@ -1,18 +1,54 @@
 import styled, { css } from 'styled-components'
 import logoImg from '@/shared/assets/logos/logo.png'
+import logoTextImg from '@/shared/assets/logos/logo_text.gif'
 import { Button } from '@/shared/ui/atoms/Button';
+import { Anchor } from '@/shared/ui/atoms/Anchor';
 
 type Props = {
   isAuthenticated: boolean;
 };
 
-const LogoImg = styled.img`
+const LogoWrapperAnchor = styled(Anchor)`
+  display: inline-flex;
+  justify-content: left;
+  align-items: center;
   margin-right: auto;
-  max-height: 48px;
   
+  cursor: pointer;
+  * {
+    pointer-events: none;
+    user-select: none;
+  }
+
   ${ ({ theme }) => css`
+    column-gap: ${ theme.spacing.xs.pc };
+    
     @media (max-width: ${ theme.breakpoints.sp}) {
-      max-height: 42px;
+      column-gap: ${ theme.spacing.xxs.sp };
+    }
+  ` }
+`;
+
+const LogoImg = styled.img`
+  ${ ({ theme }) => css`
+    max-height: 46px;
+
+    @media (max-width: ${ theme.breakpoints.sp}) {
+      max-height: 38px;
+    }
+  ` }
+`;
+
+const LogoTextImg = styled.img`
+  ${ ({ theme }) => css`
+    max-height: 36px;
+
+    @media (max-width: ${ theme.breakpoints.sp}) {
+      max-height: 30px;
+    }
+
+    @media (max-width: ${ theme.breakpoints.min}) {
+      display: none;
     }
   ` }
 `;
@@ -27,7 +63,7 @@ const HeaderWrapper = styled.header`
     const { color, spacing } = theme;
 
     return css`
-      background-color: ${ color.secondary };
+      background-color: ${ color.primary };
       padding: ${ `${ spacing.sm.pc } ${ spacing.lg.pc }` };
       
       @media (max-width: ${ theme.breakpoints.sp}) {
@@ -39,7 +75,10 @@ const HeaderWrapper = styled.header`
 
 export const Header = (props: Props) => (
   <HeaderWrapper>
-    <LogoImg src={ logoImg } alt="ロゴ画像はAI生成です" />
+    <LogoWrapperAnchor href='/'>
+      <LogoImg src={ logoImg } alt="ロゴ画像はAI生成です" />
+      <LogoTextImg src={ logoTextImg } alt="ロゴテキストのアニメーションGIFです" />
+    </LogoWrapperAnchor>
 
     <>
       {
