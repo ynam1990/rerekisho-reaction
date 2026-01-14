10ddb9757deda43e4d05ebe8bb6b35eac3eca355
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-14 13:57:57 +0900

    [update] LabelのStoryの修正

--- app/client/src/shared/ui/atoms/Label/Label.stories.tsx
+++ app/client/src/shared/ui/atoms/Label/Label.stories.tsx
@@ -1,17 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Label } from './Label';
 import { Input } from '@/shared/ui/atoms/Input'
+import { useState } from 'react';
 
 const meta: Meta<typeof Label> = {
   title: 'Atoms/Label',
   component: Label,
   args: {
-    children: (
-    <>
-      サンプル
-      <Input />
-    </>
-    ),
   },
 };
 
@@ -19,4 +14,15 @@ export default meta;
 
 type Story = StoryObj<typeof Label>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => {
+    const [value, setValue] = useState('sample');
+
+    return (
+      <Label { ...args} >
+        サンプル
+        <Input value={ value } onChange={ (e) => setValue(e.target.value) } />
+      </Label>
+    );
+  },
+};
