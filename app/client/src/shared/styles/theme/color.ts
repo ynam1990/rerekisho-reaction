bf0f24bc924efc54d4fb3aee9f0b24b2b4e69849
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-14 12:57:57 +0900

    [update] 色の追加

--- app/client/src/shared/styles/theme/color.ts
+++ app/client/src/shared/styles/theme/color.ts
@@ -2,6 +2,7 @@ export const color = {
   white: '#ffffff',
   paperWhite: '#fbfbfd',
   backgroundGray: '#f5f5f5',
+  paleGray: '#9f9f9f',
   inkBlack: '#111111',
   primary: '#3c5f9c',
   secondary: '#3f7a5a',
