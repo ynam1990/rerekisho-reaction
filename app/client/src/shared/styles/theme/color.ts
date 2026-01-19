4ffbf66ca22258fdb6b1bde9ad4e2dfa6cf42731
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-19 14:24:17 +0900

    [update] borderGrayの追加

--- app/client/src/shared/styles/theme/color.ts
+++ app/client/src/shared/styles/theme/color.ts
@@ -2,6 +2,7 @@ export const color = {
   white: '#ffffff',
   paperWhite: '#fbfbfd',
   backgroundGray: '#f5f5f5',
+  borderGray: '#dfdfdf',
   paleGray: '#9f9f9f',
   inkBlack: '#111111',
   primary: '#3c5f9c',
