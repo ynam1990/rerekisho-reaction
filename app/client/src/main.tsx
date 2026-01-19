5ed070aa2e6dac30b085946c815feb0cf7f695ee
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-19 15:15:40 +0900

    [add] Dayjsのインストール

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client'
 import '../node_modules/destyle.css'
 import { StoreProvider } from './app/providers/StoreProvider'
 import { App } from './app/App';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
+
+dayjs.locale('ja');
 
 createRoot(document.getElementById('root')!).render(
   <StoreProvider>
