faf30599b0a6aba87a0fb49ee05ca70f6d4a27f7
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-23 22:34:49 +0900

    [update] format郵便番号の修正

--- app/client/src/shared/utils/format.ts
+++ app/client/src/shared/utils/format.ts
@@ -1,5 +1,5 @@
 export const formatPostalCode = (postalCode: string) => {
-  if (postalCode.length !== 7) return postalCode;
+  if (postalCode.length <= 3) return postalCode;
   
   return `${postalCode.slice(0, 3)}-${postalCode.slice(3)}`;
 };
