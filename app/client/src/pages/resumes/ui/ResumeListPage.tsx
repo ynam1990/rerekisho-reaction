4ca0e55f07750f586f5780c0e864f5e3b62a0a30
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-29 22:31:57 +0900

    [update] 履歴書一覧取得APIとの接続

--- app/client/src/pages/resumes/ui/ResumeListPage.tsx
+++ app/client/src/pages/resumes/ui/ResumeListPage.tsx
@@ -1,9 +1,30 @@
-import { ResumeList } from "@/features/resume";
+import { useEffect } from "react";
 import { ResumeListPageWrapper } from "./ResumeListPage.styles"
-import { useResumeSelector } from "@/app/store/hooks";
+import { ResumeList } from "@/features/resume";
+import { useAppDispatch, useResumeSelector } from "@/app/store/hooks";
+import { getResumesThunk } from "@/features/resume/model/resumeThunks";
+import { useToast } from "@/shared/hooks/useToast";
+import { hasMessage } from "@/shared/utils/check";
 
 export const ResumeListPage = () => {
   const { resumeList } = useResumeSelector();
+  const showToastWithOptions = useToast();
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const fetchResumes = async () => {
+      try {
+        await dispatch(getResumesThunk()).unwrap();
+      } catch (error) {
+        showToastWithOptions({
+          icon: 'error',
+          content: (hasMessage(error) && error.message) || '履歴書一覧の取得に失敗しました。',
+        });
+      }
+    };
+    
+    fetchResumes();
+  }, []);
 
   return (
     <ResumeListPageWrapper>
