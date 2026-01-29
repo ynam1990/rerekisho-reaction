3ece70fd4637a37d2bf6544658546789b3319bdb
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-29 22:17:06 +0900

    [add] 履歴書APIをまとめたresumeThunksの追加

---
+++ app/client/src/features/resume/model/resumeThunks.ts
@@ -0,0 +1,120 @@
+import { callAPI } from "@/shared/api/request";
+import type { ResumeListItem, ResumeObj, SuccessResponse } from "@/shared/api/types";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import type { GetResumesPair, GetResumePair, PostResumePair, PutResumePair, DeleteResumePair } from "@/shared/api/types";
+
+export const getResumesThunk = createAsyncThunk<
+  ResumeListItem[],
+  void
+>(
+  'resume.getResumesThunk',
+  async (_, thunkAPI) => {
+    const { promise } = callAPI<GetResumesPair>(
+      '/resumes',
+      {
+        method: 'GET',
+      }
+    );
+
+    return await promise.then((response) => {
+      // 履歴書一覧の取得成功
+      return response;
+    }).catch((error) => {
+      // 履歴書一覧の取得失敗
+      return thunkAPI.rejectWithValue({ message: error.message });
+    });
+  }
+);
+
+export const getResumeThunk = createAsyncThunk<
+  ResumeObj,
+  { resumeId: string; }
+>(
+  'resume.getResumeThunk',
+  async (payload, thunkAPI) => {
+    const { promise } = callAPI<GetResumePair>(
+      `/resumes/${ payload.resumeId }`,
+      {
+        method: 'GET',
+      }
+    );
+
+    return await promise.then((response) => {
+      // 履歴書の取得成功
+      return response;
+    }).catch((error) => {
+      // 履歴書の取得失敗
+      return thunkAPI.rejectWithValue({ message: error.message });
+    });
+  }
+);
+
+export const postResumeThunk = createAsyncThunk<
+  SuccessResponse,
+  void
+>(
+  'resume.postResumeThunk',
+  async (_, thunkAPI) => {
+    const { promise } = callAPI<PostResumePair>(
+      '/resumes',
+      {
+        method: 'POST',
+      }
+    );
+
+    return await promise.then((response) => {
+      // 履歴書の作成成功
+      return response;
+    }).catch((error) => {
+      // 履歴書の作成失敗
+      return thunkAPI.rejectWithValue({ message: error.message });
+    });
+  }
+);
+
+export const putResumeThunk = createAsyncThunk<
+  SuccessResponse,
+  { resumeId: string; resumeData: ResumeObj; }
+>(
+  'resume.putResumeThunk',
+  async (payload, thunkAPI) => {
+    const { promise } = callAPI<PutResumePair>(
+      `/resumes/${ payload.resumeId }`,
+      {
+        method: 'PUT',
+        body: payload.resumeData,
+      }
+    );
+
+    return await promise.then((response) => {
+      // 履歴書の更新成功
+      return response;
+    }).catch((error) => {
+      // 履歴書の更新失敗
+      return thunkAPI.rejectWithValue({ message: error.message });
+    });
+  }
+);
+
+export const deleteResumeThunk = createAsyncThunk<
+  SuccessResponse,
+  { resumeId: string; }
+>(
+  'resume.deleteResumeThunk',
+  async (payload, thunkAPI) => {
+    const { promise } = callAPI<DeleteResumePair>(
+      `/resumes/${ payload.resumeId }`,
+      {
+        method: 'DELETE',
+      }
+    );
+
+    return await promise.then((response) => {
+      // 履歴書の削除成功
+      return response;
+    }).catch((error) => {
+      // 履歴書の削除失敗
+      return thunkAPI.rejectWithValue({ message: error.message });
+    });
+  }
+);
