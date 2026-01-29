cdab55163b0937c3c04969507a14c53e62279b23
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-01-29 11:30:58 +0900

    [add] errorオブジェクトがmessageを持っているか判定するhasMessageの追加

--- app/client/src/shared/utils/check.ts
+++ app/client/src/shared/utils/check.ts
@@ -1,3 +1,12 @@
 export const isUndefined = <T>(value: T | undefined): value is undefined => {
   return typeof value === 'undefined';
 };
+
+export const hasMessage = (error: unknown): error is { message: string } => {
+  return (
+    typeof error === 'object'
+    && error !== null
+    && 'message' in error
+    && typeof (error as any).message === 'string'
+  );
+};
