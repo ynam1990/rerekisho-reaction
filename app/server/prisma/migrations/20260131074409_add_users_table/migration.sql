bb381aafb8db64537c056023ff114c874db7b85f
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-02-01 12:34:15 +0900

    [add] db定義とprismaマイグレーションファイルの追加

---
+++ app/server/prisma/migrations/20260131074409_add_users_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NULL,
+    `password_hash` VARCHAR(255) NOT NULL,
+    `last_login_at` DATETIME(3) NOT NULL,
+    `last_active_at` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
