83d6fc871f71cd224a4b8a62d3261083066abbdd
ynam1990 <yn@ynnoMacBook-Air.AirPort>
2026-02-04 09:27:36 +0900

    [fix] DBパスワード等の分解中にパーセントエンコードが含まれる可能性への対応

--- app/server/src/constants/env.const.ts
+++ app/server/src/constants/env.const.ts
@@ -5,11 +5,11 @@ export const APP_PORT = Number(process.env.APP_PORT ?? '3000');
 
 export const DATABASE_URL = String(process.env.DATABASE_URL);
 const databaseUrl = new URL(DATABASE_URL);
-export const DB_HOST = databaseUrl.hostname;
-export const DB_PORT = Number(databaseUrl.port);
-export const DB_USER = databaseUrl.username;
-export const DB_PASSWORD = databaseUrl.password;
-export const DB_NAME = databaseUrl.pathname.slice(1);
+export const DB_HOST = decodeURIComponent(databaseUrl.hostname);
+export const DB_PORT = Number(decodeURIComponent(databaseUrl.port));
+export const DB_USER = decodeURIComponent(databaseUrl.username);
+export const DB_PASSWORD = decodeURIComponent(databaseUrl.password);
+export const DB_NAME = decodeURIComponent(databaseUrl.pathname.slice(1));
 
 export const SESSION_SECRET = String(process.env.SESSION_SECRET);
 export const SESSION_NAME = process.env.SESSION_NAME ?? 'connect.sid';
