edb2446e6ca8a8be6e0fea95b3b844a3bd5b58f9
ynam1990 <ynam1990+xcode@gmail.com>
2026-02-14 14:44:59 +0900

    [update] eslintのルール設定を追加

--- app/client/eslint.config.js
+++ app/client/eslint.config.js
@@ -4,21 +4,37 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import storybook from "eslint-plugin-storybook";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-async-promise-executor': 'error',
+    },
   },
-])
+  {
+    files: ['**/*.stories.{ts,tsx}', '**/*.test.{ts,tsx}'],
+    rules: {
+      // Storybook、テストは分離
+      'react-hooks/rules-of-hooks': 'off',
+    },
+  },
+]);
